ba0a79b34916378ff69da421c56c074c2b002c0f
ABC <122415117+acarpenter1212@users.noreply.github.com>
2023-01-20 13:58:09 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,3 +7,13 @@ function chapter2 () {
 function chapter1 () {
 	
 }
+let chooseChapter = game.askForString("what chapter do you want to read?")
+if (chooseChapter == "chapter 1") {
+    chapter1()
+} else if (chooseChapter == "chapter 2") {
+    chapter2()
+} else if (chooseChapter == "chapter 3") {
+    chapter3()
+} else {
+    game.splash("please retype your answer as \"chapter 1\",\"chapter 2\", or \"chapter 3\"")
+}
